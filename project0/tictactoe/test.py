617c9e45ae3fc057cc7a5801bf386c704e77535c
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-01-13 21:46:35 +0300

    tictactoe update

--- project0/tictactoe/test.py
+++ project0/tictactoe/test.py
@@ -1,7 +1,18 @@
-from tictactoe import player, actions, result, winner
+from tictactoe import player, actions, result, winner, terminal, utility
 
 EMPTY = None
 
-print(winner([["X", "X", "O"],
-              ["O", "O", EMPTY],
-              ["O", "X", "X"]]))
+board = [["O", EMPTY, "X"],
+         ["X", "O", "O"],
+         ["X", EMPTY, EMPTY]]
+
+actions = actions(board)
+utils = {}
+for action in actions:
+    utils[action] = -2
+print(utils)
+
+utils[(2, 1)] = 1
+print(utils)
+
+print(max(utils, key=lambda key: utils[key]))
--- project0/tictactoe/tictactoe.py
+++ project0/tictactoe/tictactoe.py
@@ -10,6 +10,34 @@ O = "O"
 EMPTY = None
 
 
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+class QueueFrontier():
+    def __init__(self):
+        self.frontier = []
+    
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+
+
 def initial_state():
     """
     Returns starting state of the board.
@@ -83,17 +111,17 @@ def winner(board):
 
     # Check columns
     boardFlipped = [[col[i] for col in board] for i in range(3)]
-    for col in boardFlipped:
-        Xcount.add(col.count("X"))
-        Ocount.add(col.count("O"))
+    for row in boardFlipped:
+        Xcount.add(row.count("X"))
+        Ocount.add(row.count("O"))
     
     # Check diagonals
-    diagonal1 = [board[i][i] for i in range(3)]
-    Xcount.add(diagonal1.count("X"))
-    Ocount.add(diagonal1.count("O"))
-    diagonal2 = [board[i][2 - i] for i in range(3)]
-    Xcount.add(diagonal2.count("X"))
-    Ocount.add(diagonal2.count("O"))
+    diagonalLR = [board[i][i] for i in range(3)]
+    Xcount.add(diagonalLR.count("X"))
+    Ocount.add(diagonalLR.count("O"))
+    diagonalRL = [board[i][2 - i] for i in range(3)]
+    Xcount.add(diagonalRL.count("X"))
+    Ocount.add(diagonalRL.count("O"))
 
     # Get winner
     if 3 in Xcount:
@@ -108,18 +136,66 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) != None or len(actions(board)) == 0:
+        return True
+    return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    w = winner(board)
+    if w == "X":
+        return 1
+    elif w == "O":
+        return -1
+    return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+
+    """
+    Pseudocode:
+    1. Check for terminal board.
+    2. Initialize frontier with a starting node.
+    3. Loop:
+        - check if frontier is empty (every action explored)
+            if it is empty return action with max utility
+        - 
+        
+    """
+    if terminal(board):
+        return None
+
+    # Starting node
+    start = Node(state=board, parent=None, action=None)
+
+    # Initialize frontier with a starting node
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    # Initialize utils dictionary that contains utility value for every action
+    utils = {}
+
+    # Loop until action is found
+    while True:
+
+        # If nothing left in frontier, return action
+        if frontier.empty():
+            # Get action with max/min utility
+            if player(board) == "X":
+                action = max(utils, key=lambda key: utils[key])
+            else:
+                action = min(utils, key=lambda key: utils[key])
+            return action
+
+        # Remove node
+        node = frontier.remove()
+        
+        # Add actions to frontier
+        for action in actions(board):
+            
