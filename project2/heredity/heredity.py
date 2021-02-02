9d3938c9bf9fc6fb76ec5b90c8c8c05fce9b6169
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-02-02 20:05:19 +0300

    finish heredity

--- project2/heredity/heredity.py
+++ project2/heredity/heredity.py
@@ -2,6 +2,8 @@ import csv
 import itertools
 import sys
 
+import numpy as np
+
 PROBS = {
 
     # Unconditional probabilities for having gene
@@ -62,8 +64,6 @@ def main():
 
     # Loop over all sets of people who might have the trait
     names = set(people)
-    joint_probability(people, {"Harry"}, {"James"}, {"James"})
-    return
     for have_trait in powerset(names):
 
         # Check if current set of people violates known information
@@ -141,17 +141,24 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    # Initialize a dictionary that contains gene and traits info for every person
-    data = {}
+    # Initialize a dictionary that contains gene and trait info for every person
+    data = dict()
     for name in people:
         genes = 1 * (name in one_gene) + 2 * (name in two_genes)
         trait = name in have_trait
 
-        data[name]["genes"] = genes
-        data[name]["trait"] = trait
+        data[name] = {
+            "genes": genes,
+            "trait": trait
+            }
 
+    # Initialize a dictionary that will contain probability for every person
     probabilities = {}
+
+    # Loop over every person and calculate his probability
     for name in people:
+
+        # Get parents, genes and trait of person
         mother = people[name]["mother"]
         father = people[name]["father"]
         genes = data[name]["genes"]
@@ -163,38 +170,55 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         # Gene probability if has parents
         if mother:
             """
-            0: no one           : (1 - mother gave) * (1 - father gave)
-            1: mother or father : mother gave * (1 - father gave) + (1 - mother gave) * father gave
-            2: both             : mother gave * father gave
+            Probability calculation logic if person has parents:
+
+            Child has 0 genes -> no one passed a gene:
+                probability = (1 - mother passed) * (1 - father passed)
+            Child has 1 gene  -> mother or father passed a gene:
+                probability = mother passed * (1 - father passed) + 
+                              (1 - mother passed) * father passed
+            Child has 2 genes -> mother and father passed a gene:
+                probability = mother passed * father passed
             
-            parent[0] gave = 0.01
-            parent[1] gave = 0.5 * (1 - mutation)
-            parent[2] gave = 1 - mutation
+            parent with 0 genes passed = mutation probability
+            parent with 1 gene passed  = 0.5
+            parent with 2 genes passed = 1 - mutation probability
+            """
+
+            """
+            Initialize a dictionary that will contain probability of 
+            passing a gene for parents.
             """
             parentProbs = {}
+
+            # Calculate probabilities for parents
             for parent in (mother, father):
                 parentGenes = data[parent]["genes"]
                 parentProbs[parent] = (
                     PROBS["mutation"] if parentGenes == 0 else
-                    0.5 * (1 - PROBS["mutation"]) if parentGenes == 1 else
+                    0.5 if parentGenes == 1 else
                     1 - PROBS["mutation"]
                 )
             gotFromMother = parentProbs[mother]
             gotFromFather = parentProbs[father]
 
+            # Calculate gene probability and muliply on trait probability
             if genes == 0:
-                probabilities[name] += (1 - gotFromMother) * (1 - gotFromFather)
+                probabilities[name] *= (1 - gotFromMother) * (1 - gotFromFather)
             elif genes == 1:
-                probabilities[name] += (
-                    gotFromMother) * (1 - gotFromFather) + \
+                probabilities[name] *= (
+                    gotFromMother * (1 - gotFromFather) + \
                     (1 - gotFromMother) * gotFromFather
+                    )
             else:
-                probabilities[name] += gotFromMother * gotFromFather
+                probabilities[name] *= gotFromMother * gotFromFather
+
         # Gene probability if no parents
         else:
             probabilities[name] *= PROBS["gene"][genes]
-    
-    print(probabilities)
+
+    # Multiply probabilities of every person and return value
+    return np.prod(np.array(list(probabilities.values())))
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
@@ -204,7 +228,16 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    # Loop over every person
+    for person in probabilities:
+
+        # Get genes and trait of person
+        genes = 1 * (person in one_gene) + 2 * (person in two_genes)
+        trait = person in have_trait
+
+        # Change value
+        probabilities[person]["gene"][genes] += p
+        probabilities[person]["trait"][trait] += p
 
 
 def normalize(probabilities):
@@ -212,7 +245,19 @@ def normalize(probabilities):
     Update `probabilities` such that each probability distribution
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
-    raise NotImplementedError
+    # Loop over every person
+    for person in probabilities:
+
+        # Loop over genes/traits
+        for values in probabilities[person]:
+
+            # Calculate alpha
+            alpha = 1 / sum(list(probabilities[person][values].values()))
+
+            # Loop over every `genes`/`trait` value and multiply it by `alpha`
+            for value in probabilities[person][values]:
+                probability = probabilities[person][values][value]
+                probabilities[person][values][value] = alpha * probability
 
 
 if __name__ == "__main__":
