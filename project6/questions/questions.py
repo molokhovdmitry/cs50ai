e8356889b42284c597cdf347d428d8f81d3dec2f
Dmitry Molokhov <molokhovdmitry@gmail.com>
2021-02-25 21:13:43 +0300

    Finish questions

--- project6/questions/questions.py
+++ project6/questions/questions.py
@@ -2,6 +2,8 @@ import nltk
 import sys
 
 import os
+import string
+import math
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -50,13 +52,23 @@ def load_files(directory):
     Given a directory name, return a dictionary mapping the filename of each
     `.txt` file inside that directory to the file's contents as a string.
     """
-    docs = {}
+    files = dict()
+    # Iterate over files in directory
     for doc in os.listdir(directory):
+
+        # Get path of file
         path = os.path.join(directory, doc)
+
+        # Open file
         with open(path) as reader:
+
+            # Get filename
             name = os.path.basename(os.path.normpath(path)).rstrip(".txt")
-            docs[name] = reader.read()
-    return docs
+            
+            # Get contents
+            files[name] = reader.read()
+
+    return files
 
 
 def tokenize(document):
@@ -67,11 +79,13 @@ def tokenize(document):
     Process document by coverting all words to lowercase, and removing any
     punctuation or English stopwords.
     """
+    # Add a word if it's not in stopwords and it's not a punctuation
     words = [
         word.lower() for word in nltk.word_tokenize(document)
-        if any(c.isalpha() for c in word)
+        if word.lower() not in (nltk.corpus.stopwords.words("english") +
+                                list(string.punctuation))
     ]
-
+    
     return words
 
 def compute_idfs(documents):
@@ -81,8 +95,31 @@ def compute_idfs(documents):
 
     Any word that appears in at least one of the documents should be in the
     resulting dictionary.
+
+    idf = ln(Number of documents /
+             Number of documents in which the word appears)
     """
-    raise NotImplementedError
+
+    # Number of documents
+    docNum = len(documents)
+
+    idfs = dict()
+    for document in documents:
+        for word in documents[document]:
+            # Skip words that are already in a dict
+            if word in idfs:
+                continue
+            
+            # Calculate number of documents in which the word appears
+            wordDocNum = 0
+            for document in documents:
+                if word in documents[document]:
+                    wordDocNum += 1
+            
+            # Calculate idfs
+            idfs[word] = math.log(docNum / wordDocNum, math.e)
+    
+    return idfs
 
 
 def top_files(query, files, idfs, n):
@@ -92,7 +129,25 @@ def top_files(query, files, idfs, n):
     to their IDF values), return a list of the filenames of the the `n` top
     files that match the query, ranked according to tf-idf.
     """
-    raise NotImplementedError
+    topFiles = dict()
+    for document in files:
+
+        # Initialize document's tf-idf value
+        topFiles[document] = 0
+        for word in query:
+            if word in idfs:
+
+                # Calculate tf
+                tf = files[document].count(word)
+                
+                # Calculate tf-idf
+                tfidf = tf * idfs[word]
+
+                # Assign tf-idf value to document
+                topFiles[document] += tfidf
+
+    # Sort and return top `n` files
+    return sorted(topFiles, key=topFiles.get, reverse=True)[:n]
 
 
 def top_sentences(query, sentences, idfs, n):
@@ -103,7 +158,32 @@ def top_sentences(query, sentences, idfs, n):
     the query, ranked according to idf. If there are ties, preference should
     be given to sentences that have a higher query term density.
     """
-    raise NotImplementedError
+    topSent = dict()
+    for sentence in sentences:
+
+        # Initialize sentence's `idf` and query term density values
+        topSent[sentence] = [0, 0]
+
+        # Calculate query term density (`length of intersection` / 
+        #                               `length of sentence`)
+        qtd = (len(query.intersection(sentences[sentence])) /
+               len(set(sentences[sentence])))
+
+        # Assign `qtd` to sentence
+        topSent[sentence] = [0, qtd]
+
+        # Add word's `idf` values to sentence if word in sentence and in `idf`
+        for word in query:
+            if word in sentences[sentence] and word in idfs:
+                topSent[sentence][0] += idfs[word]
+
+    # Sort sentences by `idf`, then by `qtd`
+    topSent = sorted(topSent,
+                     key=lambda key: (topSent[key][0], topSent[key][1]),
+                     reverse=True)
+
+    # Return top `n` sentences
+    return topSent[:n]
 
 
 if __name__ == "__main__":
